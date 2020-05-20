63492f1dd49edc91890e242fdb6d61355f5b6870
Kevin Loughead <kvnloughead@gmail.com>
2020-05-20 18:53:33 -0400

    (feature) new-place-modal:
    New place modal now appears when the add button is clicked, and
    closes with the close button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,34 @@
 const editBtn = document.querySelector('.button_action_edit');
-const editPopup = document.querySelector('.popup');
-const closeBtn = editPopup.querySelector('.button_action_close');
-const saveBtn = editPopup.querySelector('.button_action_submit');
+const popup = document.querySelector('.popup');
+const editModal = popup.querySelector('.popup__container_type_edit');
+const addModal = popup.querySelector('.popup__container_type_new-place');
+const closeEditBtn = popup.querySelectorAll('.button_action_close')[0];
+const closeNewBtn = popup.querySelectorAll('.button_action_close')[1];
+const saveBtn = popup.querySelector('.button_action_submit');
 const addBtn = document.querySelector('.button_action_add');
 
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
 
-const popup = document.querySelector('.popup');
 const formElement = popup.querySelector('.popup__form');
 const [name, job] = popup.querySelectorAll('.popup__input');
 
 function toggleOverlayAndEditModal() {
   name.value = profileName.textContent;
   job.value = profileJob.textContent;
-  editPopup.classList.toggle('popup_visible');
+  popup.classList.toggle('popup_visible');
 }
 
 function toggleOverlayAndModal(evt) {
   const btnClassList = Array.from(evt.target.classList);
-  console.log(btnClassList);
+  popup.classList.toggle('popup_visible');
   if (btnClassList.includes('button_action_add')) {
-    
-    editPopup.classList.toggle('popup_visible');
+    editModal.classList.add('popup__container_invisible');
+    addModal.classList.remove('popup__container_invisible');
+  } else if (btnClassList.includes('button_action_edit')){
+    addModal.classList.add('popup__container_invisible');
+    editModal.classList.remove('popup__container_invisible');
   }
 }
 
@@ -34,7 +39,8 @@ function formSubmitHandler(evt) {
   toggleOverlayAndEditModal();
 }
 
-editBtn.addEventListener('click', toggleOverlayAndEditModal);
-closeBtn.addEventListener('click', toggleOverlayAndEditModal);
-formElement.addEventListener('submit', formSubmitHandler);
+editBtn.addEventListener('click', toggleOverlayAndModal);
 addBtn.addEventListener('click', toggleOverlayAndModal);
+closeEditBtn.addEventListener('click', toggleOverlayAndModal);
+closeNewBtn.addEventListener('click', toggleOverlayAndModal);  
+formElement.addEventListener('submit', formSubmitHandler);

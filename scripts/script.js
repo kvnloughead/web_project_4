504fcd263b033fe6b14e32e8d9e5873f44157b2a
Kevin Loughead <kvnloughead@gmail.com>
2020-05-20 20:41:56 -0400

    (feature) new-place-modal: Now fully functioning, allowing you to create new places dynamically.

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,14 @@ const editModal = popup.querySelector('.popup__container_type_edit');
 const addModal = popup.querySelector('.popup__container_type_new-place');
 const closeEditBtn = popup.querySelectorAll('.button_action_close')[0];
 const closeNewBtn = popup.querySelectorAll('.button_action_close')[1];
-const saveBtn = popup.querySelector('.button_action_submit');
 const addBtn = document.querySelector('.button_action_add');
 
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
 
-const formElement = popup.querySelector('.popup__form');
+const editFormElement = popup.querySelectorAll('.popup__form')[0];
+const addFormElement = popup.querySelectorAll('.popup__form')[1];
 const [name, job] = popup.querySelectorAll('.popup__input');
 
 function toggleOverlayAndEditModal() {
@@ -39,8 +39,34 @@ function formSubmitHandler(evt) {
   toggleOverlayAndEditModal();
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  
+  const listItem = document.createElement('li');
+  const image = document.createElement('div');
+  const footer = document.createElement('div');
+  const name = document.createElement('h2');
+  const btn = document.createElement('button');
+
+  listItem.classList.add('place');
+  image.classList.add('place__image');
+  footer.classList.add('place__footer');
+  name.classList.add('place__name');
+  btn.classList.add('button', 'button_action_like');
+
+  image.style.backgroundImage = `url(${imageUrl.value})`;
+  name.textContent = title.value;
+
+  footer.append(name, btn);
+  listItem.append(image, footer);
+  placesGrid.prepend(listItem);
+
+  toggleOverlayAndEditModal();
+}
+
 editBtn.addEventListener('click', toggleOverlayAndModal);
 addBtn.addEventListener('click', toggleOverlayAndModal);
 closeEditBtn.addEventListener('click', toggleOverlayAndModal);
-closeNewBtn.addEventListener('click', toggleOverlayAndModal);  
-formElement.addEventListener('submit', formSubmitHandler);
+closeNewBtn.addEventListener('click', toggleOverlayAndModal)  
+editFormElement.addEventListener('submit', formSubmitHandler);
+addFormElement.addEventListener('submit', formSubmitHandler);

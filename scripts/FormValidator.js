adc0f2d9a8d436531ff3274a8d38df678d594f45
Kevin Loughead <kvnloughead@gmail.com>
2020-06-22 11:23:48 -0400

    Defactored non-validation functionality to index.js.   Validation/submission currently not working.

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,8 +1,4 @@
-import { Card } from "./Card.js";
-
-export const modalOverlay = document.querySelector(".popup__modal-overlay");
-const profileName = document.querySelector(".profile__name");
-const profileJob = document.querySelector(".profile__job");
+// import { Card } from "./Card.js";
 
 export class FormValidator {
   constructor(settingsObject, formToValidate) {
@@ -74,82 +70,82 @@ export class FormValidator {
     });
   }
 
-  _setInputValues() {
-    const [currName, currJob] = Array.from(
-      this._form.querySelectorAll(this._args.inputSelector)
-    );
-    currName.value = profileName.textContent;
-    currJob.value = profileJob.textContent;
-  }
+  // _setInputValues() {
+  //   const [currName, currJob] = Array.from(
+  //     this._form.querySelectorAll(this._args.inputSelector)
+  //   );
+  //   currName.value = profileName.textContent;
+  //   currJob.value = profileJob.textContent;
+  // }
 
-  _closePopup() {
-    this._container.classList.remove("popup__container_visible");
-    modalOverlay.classList.remove("popup__modal-overlay_visible");
-    for (const input of this._inputList) {
-      this._hideInputError(input);
-    }
-    this._form.reset();
-    if (this._form.id === "edit-form") {
-      this._setInputValues();
-    }
-  }
+  // _closePopup() {
+  //   this._container.classList.remove("popup__container_visible");
+  //   modalOverlay.classList.remove("popup__modal-overlay_visible");
+  //   for (const input of this._inputList) {
+  //     this._hideInputError(input);
+  //   }
+  //   this._form.reset();
+  //   if (this._form.id === "edit-form") {
+  //     this._setInputValues();
+  //   }
+  // }
 
-  _editFormSubmitHandler(evt) {
-    evt.preventDefault();
-    const newName = evt.currentTarget.name.value;
-    const newJob = evt.currentTarget.job.value;
-    profileName.textContent = newName;
-    profileJob.textContent = newJob;
-    this._closePopup();
-  }
+  // _editFormSubmitHandler(evt) {
+  //   evt.preventDefault();
+  //   const newName = evt.currentTarget.name.value;
+  //   const newJob = evt.currentTarget.job.value;
+  //   profileName.textContent = newName;
+  //   profileJob.textContent = newJob;
+  //   this._closePopup();
+  // }
 
-  _createEditFormSubmitListener() {
-    this._setInputValues();
-    this._form.addEventListener("submit", (evt) => {
-      this._editFormSubmitHandler(evt);
-      this._setInputValues();
-    });
-  }
+  // _createEditFormSubmitListener() {
+  //   this._setInputValues();
+  //   this._form.addEventListener("submit", (evt) => {
+  //     this._editFormSubmitHandler(evt);
+  //     this._setInputValues();
+  //   });
+  // }
 
-  _createNewFormSubmitListener() {
-    this._form.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-      const title = this._inputList[0].value;
-      const imageUrl = this._inputList[1].value;
-      const newCard = new Card(title, imageUrl, this._args.cardSelector);
-      newCard.generateCard();
-      this._closePopup();
-      evt.target.reset();
-    });
-  }
+  // _createNewFormSubmitListener() {
+  //   this._form.addEventListener("submit", (evt) => {
+  //     evt.preventDefault();
+  //     const title = this._inputList[0].value;
+  //     const imageUrl = this._inputList[1].value;
+  //     const newCard = new Card(title, imageUrl, this._args.cardSelector);
+  //     newCard.generateCard();
+  //     this._closePopup();
+  //     evt.target.reset();
+  //   });
+  // }
 
-  _addEscapeKeyListener() {
-    document.addEventListener("keydown", (evt) => {
-      if (
-        evt.key === "Escape" &&
-        this._container.classList.contains("popup__container_visible")
-      ) {
-        this._closePopup();
-      }
-    });
-  }
+  // _addEscapeKeyListener() {
+  //   document.addEventListener("keydown", (evt) => {
+  //     if (
+  //       evt.key === "Escape" &&
+  //       this._container.classList.contains("popup__container_visible")
+  //     ) {
+  //       this._closePopup();
+  //     }
+  //   });
+  // }
 
-  _addPopupOverlayListener() {
-    modalOverlay.addEventListener("click", () => {
-      if (this._container.classList.contains("popup__container_visible")) {
-        this._closePopup();
-      }
-    });
-  }
+  // _addPopupOverlayListener() {
+  //   modalOverlay.addEventListener("click", () => {
+  //     if (this._container.classList.contains("popup__container_visible")) {
+  //       this._closePopup();
+  //     }
+  //   });
+  // }
 
-  _addCloseBtnEventListener() {
-    const closeBtn = this._container.querySelector(
-      this._args.closeButtonSelector
-    );
-    closeBtn.addEventListener("click", () => {
-      this._closePopup();
-    });
-  }
+  // _addCloseBtnEventListener() {
+  //   const closeBtn = this._container.querySelector(
+  //     this._args.closeButtonSelector
+  //   );
+  //   closeBtn.addEventListener("click", () => {
+  //     this._closePopup();
+  //   });
+  // }
 
   enableValidation() {
     this._toggleFormActiveState();
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 import { Card } from './Card.js';
-import { modalOverlay, FormValidator } from './FormValidator.js';
+import { FormValidator } from './FormValidator.js';
 
 const cardSelector = '#card-template';
 const editModalTemplate = document.querySelector('#edit-modal-template');
 const addModalTemplate = document.querySelector('#add-modal-template');
 const editBtn = document.querySelector('.button_action_edit');
 const addBtn = document.querySelector('.button_action_add');
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+const modalOverlay = document.querySelector(".popup__modal-overlay");
+// const forms = document.querySelectorAll('.popup__form');
+
+
 
 const modalArgs = {
   formSelector: ".popup__form",
@@ -59,7 +65,101 @@ function createModal(modalTemplate, buttonElement) {
     openModalPopup(container);
   });
   editBtn.parentNode.appendChild(cloneOfTemplate);
+  const addForm = document.querySelector('#add-form');
+  const editForm = document.querySelector('#edit-form');
+  if (modalTemplate.id === 'edit-modal-template') {
+    createEditFormSubmitListener(editForm, container, modalArgs);
+    addCloseBtnEventListener(editForm, container, modalArgs);
+    addPopupOverlayListener(editForm, container, modalArgs);
+    addEscapeKeyListener(editForm, container, modalArgs);
+  } else {
+    createNewFormSubmitListener(addForm, container, modalArgs);
+    addCloseBtnEventListener(addForm, container, modalArgs);
+    addPopupOverlayListener(addForm, container, modalArgs);
+    addEscapeKeyListener(addForm, container, modalArgs);
+  }
 }
+
+// ======================================================
+
+function setInputValues(form, args) {
+  const [currName, currJob] = Array.from(
+    form.querySelectorAll(args.inputSelector)
+  );
+  currName.value = profileName.textContent;
+  currJob.value = profileJob.textContent;
+}
+
+function closePopup(form, container, args) {
+  container.classList.remove("popup__container_visible");
+  modalOverlay.classList.remove("popup__modal-overlay_visible");
+  // for (const input of this._inputList) {
+  //   this._hideInputError(input);
+  // }
+  // form.reset();
+  if (form.id === "edit-form") {
+    setInputValues(form, args);
+  }
+}
+
+function editFormSubmitHandler(evt, form, container, args) {
+  evt.preventDefault();
+  const newName = evt.currentTarget.name.value;
+  const newJob = evt.currentTarget.job.value;
+  profileName.textContent = newName;
+  profileJob.textContent = newJob;
+  closePopup(form, container, args);
+}
+
+function createEditFormSubmitListener(form, container, args) {
+  setInputValues(form, args);
+  form.addEventListener("submit", (evt) => {
+    editFormSubmitHandler(evt, container, args);
+    setInputValues(form, args);
+  });
+}
+
+function createNewFormSubmitListener(form, args) {
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const title = inputList[0].value;
+    const imageUrl = inputList[1].value;
+    const newCard = new Card(title, imageUrl, this._args.cardSelector);
+    newCard.generateCard();
+    closePopup(form, container, args);
+    evt.target.reset();
+  });
+}
+
+function addEscapeKeyListener(form, container, args) {
+  document.addEventListener("keydown", (evt) => {
+    if (
+      evt.key === "Escape" &&
+      container.classList.contains("popup__container_visible")
+    ) {
+      closePopup(form, container, args);
+    }
+  });
+}
+
+function addPopupOverlayListener(form, container, args) {
+  modalOverlay.addEventListener("click", () => {
+    if (container.classList.contains("popup__container_visible")) {
+      closePopup(form, container, args);
+    }
+  });
+}
+
+function addCloseBtnEventListener(form, container, args) {
+  const closeBtn = container.querySelector(
+    args.closeButtonSelector
+  );
+  closeBtn.addEventListener("click", () => {
+    closePopup(form, container, args);
+  });
+}
+
+// =====================================================
 
 createModal(editModalTemplate, editBtn);
 createModal(addModalTemplate, addBtn);
@@ -69,9 +169,11 @@ for (const card of initialCards) {
   cardEl.generateCard();
 } 
 
-const forms = Array.from(document.querySelectorAll('.popup__form'));
-for (const form of forms) {
-  const validator = new FormValidator(modalArgs, form);
-  validator.enableValidation();
-}
+const addFormValidator = new FormValidator(modalArgs, addForm);
+const editFormValidator = new FormValidator(modalArgs, editForm);
+
+// for (const form of forms) {
+//   const validator = new FormValidator(modalArgs, form);
+//   validator.enableValidation();
+// }
 
